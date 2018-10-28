79e936ee1578c59009fca743f741671d6200b8a1
decentninja <encrpt.wave@gmail.com>
2018-10-29 00:09:37 +0100

    Wip refactor
    Readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,24 @@
-Calculates the stock/bond ratio of your avanza portfolio.
+Calculates the stock/bond ratio of your Avanza portfolio.
+
+Will give you an output such as:
+```
+Your portfolio consists of
+Branschfond          1215.9   1.5%
+Aktier               2194.0   2.7%
+Aktiefond           56087.5  69.2%
+Räntefond           21606.9  26.6%
+Total               81104.3
+```
+
+# Installation Instructions
+Install nodejs version > v10.12.0
+Rust 1.29.1 (2018-09-20)
+Clone the repo
+Run `cargo install`
+Run `avanza-additional-analysis`
+
+# Development Notes
+The application consists of two parts.
+* A rust executable, that handles the analysis and the userfacing commandline interface.
+* A nodejs script that that calls github user fhqvst's Avanza module
+These two parts talk with a readline interface.
---
+++ src/analysis.rs
@@ -0,0 +1,66 @@
+/// The main analysis
+
+#[derive(Default)]
+struct Stats {
+    types: HashMap<String, f64>,
+}
+
+impl Stats {
+    fn format(&self) -> String {
+        let total: f64 = self.types.values().sum();
+        let lines = self
+            .types
+            .iter()
+            .map(|(name, value)| {
+                format!(
+                    "{:13} {:>13.1}  {:>4.1}%",
+                    name,
+                    value,
+                    100. * value / total
+                )
+            }).collect::<Vec<String>>()
+            .join("\n");
+        format!(
+            "Your portfolio consists of\n{}\n{:13} {:>13.1}",
+            lines, "Total", total
+        )
+    }
+
+    fn track(&mut self, name: String, value: f64) {
+        *self.types.entry(name).or_default() += value;
+    }
+}
+
+fn calculate_stats(auth: &Auth) -> Result<Stats, serde_json::Value> {
+    let mut child = avanza_talk(&auth).map_err(|e| json!(e))?;
+    let positions = talk_command(&mut child, &["getpositions"])?;
+    let mut stats = Stats::default();
+    let mut not_supported = Vec::new();
+    for category in positions["instrumentPositions"].as_array().unwrap() {
+        match category["instrumentType"].as_str().unwrap() {
+            "STOCK" => {
+                for position in category["positions"].as_array().unwrap() {
+                    let value = position["value"].as_f64().unwrap();
+                    stats.track("Aktier".to_string(), value);
+                }
+            }
+            "FUND" => {
+                for position in category["positions"].as_array().unwrap() {
+                    let value = position["value"].as_f64().unwrap();
+                    let orderbookid = position["orderbookId"].as_str().unwrap();
+                    let instrument =
+                        talk_command(&mut child, &["getinstrument", "FUND", orderbookid])?;
+                    stats.track(instrument["type"].as_str().unwrap().to_string(), value);
+                }
+            }
+            instrument_type => not_supported.push(instrument_type),
+        }
+    }
+    if !not_supported.is_empty() {
+        println!(
+            "NOTE: The application only counts STOCK and FUND instruments, not {}.",
+            not_supported.join(", ")
+        );
+    }
+    Ok(stats)
+}
